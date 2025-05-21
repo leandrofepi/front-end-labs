5eea8d1e228ce2447547374f9bee469e97cf423f
leandrofepi <leandro.pereira@fepi.br>
2025-05-20 22:27:09 -0300

    Introducao_DOM

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,2 +1,10 @@
-let nome = "Leandro";
-alert("Boa noite, " + nome);
+const linkExterno = 
+        document.querySelector('a[title="Google Maps"]');
+
+//linkExterno.onclick = () => {
+  //  alert("Você será redirecionado para um link externo.");
+//};
+
+linkExterno.addEventListener('click', () => {
+        alert("Você será redirecionado para um link externo.");
+});
